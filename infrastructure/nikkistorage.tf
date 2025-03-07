99276360c764875cf839d3e1e30c4cf0a5aa895c
NikkiShaker <shak0039@algonquinlive.com>
2025-03-07 18:32:21 -0500

    Test 9

--- infrastructure/nikkistorage.tf
+++ infrastructure/nikkistorage.tf
@@ -1,5 +1,11 @@
 provider "azurerm" {
   features {}
+
+  # Azure Authentication
+  subscription_id = "d690b447-1036-4d22-b055-a8625e53fed5"
+  #  client_id       = "your-client-id"     # Replace with your actual client ID
+  # client_secret   = "your-client-secret" # Replace with your actual client secret
+  # tenant_id       = "your-tenant-id"     # Replace with your actual tenant ID
 }
 
 resource "azurerm_storage_account" "example" {
@@ -8,5 +14,4 @@ resource "azurerm_storage_account" "example" {
   location                 = "Canada Central"
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
-// Removed bracket
+}
