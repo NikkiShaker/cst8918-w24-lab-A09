ea6a2fc31c7ce19d7fa30e69a9f603ecb7c6ec52
NikkiShaker <shak0039@algonquinlive.com>
2025-03-07 17:41:01 -0500

    Create nikkistorage.tf

---
+++ infrastructure/nikkistorage.tf
@@ -0,0 +1,11 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "nikkistorageccount"
+  resource_group_name       = "cst8918-lab-a09"
+  location                 = "Canada Central"
+  account_tier               = "Standard"
+  account_replication_type = "LRS"
+}
