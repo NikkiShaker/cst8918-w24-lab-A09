207d1d4ca48ff9cb2ea33651b80975a8decd22c6
NikkiShaker <shak0039@algonquinlive.com>
2025-03-07 18:13:57 -0500

    Testing to see if script works

--- infrastructure/nikkistorage.tf
+++ infrastructure/nikkistorage.tf
@@ -4,9 +4,9 @@ provider "azurerm" {
 
 resource "azurerm_storage_account" "example" {
   name                     = "nikkistorageccount"
-  resource_group_name       = "cst8918-lab-a09"
+  resource_group_name      = "cst8918-lab-a09"
   location                 = "Canada Central"
-  account_tier               = "Standard"
+  account_tier             = "Standard"
   account_replication_type = "LRS"
-]]
+}
 // Removed bracket
