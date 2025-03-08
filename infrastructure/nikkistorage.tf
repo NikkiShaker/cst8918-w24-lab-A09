cf268bc5635c83be83d0a228045bde234172fbba
NikkiShaker <shak0039@algonquinlive.com>
2025-03-07 23:26:08 -0500

    Created syntax issue

--- infrastructure/nikkistorage.tf
+++ infrastructure/nikkistorage.tf
@@ -15,8 +15,7 @@ provider "azurerm" {
 resource "azurerm_storage_account" "example" {
   name                     = "nikkistorageccount"
   resource_group_name      = "cst8918-lab-a09"
-  location                 
-  = "Canada Central"
+  location                 = "Canada Central"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
